2190527cbfccb9911db83e871a3735ffa718cb53
Paul <jamespauloconnor@gmail.com>
2016-07-05 23:46:53 -0400

    Added challenges to objects file

--- 2-objectchallenges.js
+++ 2-objectchallenges.js
@@ -27,12 +27,6 @@ var netflix = {
   averageRating : 4,
   watchNow      : function(){
     alert("You are now queueing up " + this.showName + " Episode: " + this.episodeNo);
-  },
-  suggestShow   : function(){
-    if(this.averageRating >= 3.5){
-      var highlyRated = ""
-      document.getElementById("suggest-show").innerHTML = 
-    }
   }
 };
 
@@ -65,33 +59,94 @@ document.getElementById("episode-description-2").innerHTML = peakyBlinders.episo
 //Silver: Inside the netflixShow object, create another object called seasonInfo. Inside that season, create a season number property, an overview property,and a function that prints the overview.
 //Gold: Create another object literal called episodes. This object is going to be an array with information inside of it.
 // episodeNumber & episodename properties. 
-/*
+
+//A couple utility functions for making space in the console
+function makeTwoLinesOfSpace(){
+  console.log("        ");
+  console.log("        ");
+}
+
+section = "  ACCESSING THE EPISODES  "
+function utilityFunctionForMakingSpace(section){
+  for(var i = 0; i < 5; i++)
+  if(i===2 && section === "  ACCESSING DIFFERENT PARTS OF THE OBJECTS             "){
+    console.log(section)
+  }else if(i===2 && section === "  ACCESSING THE EPISODES  " ){
+    console.log("  ACCESSING THE EPISODES  ");
+  }else if(i===2 && section === "  JSON EXAMPLE       "){
+    console.log(section);
+  }else{
+    console.log("           ");
+  }
+}
+
+//Our netflix object
+
 var netflixShow = {
-  id          : 30000000,
+  id          : 1,
   name        : "Happiness Prevails in Paul's World!",
   description : "This show is the happiest show in history.",
   seasonInfo  : {
     seasonNumber : 1,
-    overview     : "In this season the main character doesn't die.",
+    overview     : "In this season the main character is happy.",
     printDetails : function(){
-      document.write(overview);
-    }
-  },
+     console.log(this.overview);
+    },
   episodes: [
     { episodeNumber: 1, episodeName: "Happy Stuff"},
     { episodeNumber: 2, episodeName: "The Real Happy Story"},
-    { episodeNumber: 3, episodeName: "Even happier than before."}
+    { episodeNumber: 3, episodeName: "Even happier than before."},
+    { episodeNumber: 4, episodeName: "Wow, so cool."},
+    { episodeNumber: 5, episodeName: "A day off."},
+    { episodeNumber: 6, episodeName: "Great."},
+    { episodeNumber: 7, episodeName: "Happiness is here to stay."},
+    { episodeNumber: 8, episodeName: "A dream achieved."},
+    { episodeNumber: 9, episodeName: "It's so great."},
+  { episodeNumber: 10, episodeName: "unbounded joy."}
   ]
+  }
 };
 
-//Accessing the object
+//Accessing parts of the object
 
-  for (info in netflixShow.seasonInfo){
-    document.write("<br/>" + info + "==>" + netflixShow.seasonInfo[info])
-  }
-*/
-//Playing around with this still.
-var movie={
+section = "  ACCESSING DIFFERENT PARTS OF THE OBJECTS             ";
+utilityFunctionForMakingSpace();
+console.log("1: " + netflixShow.id);
+console.log("2: " + netflixShow.name);
+console.log("3: " + netflixShow.description);
+console.log("4: " + netflixShow.seasonInfo.seasonNumber);
+console.log("5: " + netflixShow.seasonInfo.overview);
+console.log("6: " + netflixShow.seasonInfo.episodes[0].episodeNumber);
+console.log("7: " + "Episode: " + netflixShow.seasonInfo.episodes[0].episodeNumber + " " +  netflixShow.seasonInfo.episodes[0].episodeName);
+
+netflixShow.seasonInfo.printDetails();
+
+section = "  ACCESSING THE EPISODES  "
+utilityFunctionForMakingSpace();
+//One way to do it. 
+for (var info in netflixShow.seasonInfo.episodes){
+  console.log(netflixShow.seasonInfo.episodes[info].episodeNumber);
+}
+
+makeTwoLinesOfSpace();
+
+console.log("Printing the episode numbers and names.");
+for (var info in netflixShow.seasonInfo.episodes){
+    console.log(netflixShow.seasonInfo.episodes[info].episodeNumber + " " + netflixShow.seasonInfo.episodes[info].episodeName);
+}
+
+makeTwoLinesOfSpace();
+
+console.log("Ryan's Method - Pretty cool");
+for (info in netflixShow.seasonInfo.episodes){
+  console.log((parseInt(info) + 1) + " - " + netflixShow.seasonInfo.episodes[info].episodeName);
+}
+
+section = "  JSON EXAMPLE       "
+utilityFunctionForMakingSpace();
+
+//This is more how JSON is going to look. A bunch of strings.
+var movie = {
         details:{
         "the_shining"   :[{"full_name":"The Shining"},{"director":"Stanley Kubrick"},{"year":"1980?"}],
         "pulp_fiction"  :[{"full_name":"Pulp Fiction"},{"director":"Quentin Tarantino"},{"year":"1994"}],
@@ -99,13 +154,63 @@ var movie={
         }
     };
 
-function printMovieDetails(){
+//Notice the difference between a console.log & a stringify 
+console.log(movie.details);
+JSON.stringify(movie.details);
 
-for (var i in movie.details)
-    var container = ""
-    {
-        var movieTable = '<tr><td>' + movie.details[i][0]['full_name'] + '</td><td>' + movie.details[i][1]['director'] + '</td><td>' + movie.details[i][2]['year'] + '</td></tr>';
-        container += movieTable;
-    }
-//    document.getElementById("tablePrint").innerHTML = movieTable;
+
+
+
+//Practice with Object Literals
+
+//Bronze Challenge:
+  //Create an object called fishersYelpRestaurants object literal with the following properties
+  //create a directoryIdByZip property and set it to a zip code int.
+  //create a name property and give it a value of "Local Yelp Directory"
+  //a description that has a value of "This is a directory of restaurants in Fishers"
+  //a restaurantInfo property that is an object inside of the fishersYelpRestaurants object
+  //inside the restaurantInfo object, you should have a restaurantsByRating array
+  //the array should have another object with three properties rating, restaurantName, and topDish
+  //Assign unique values to each of these.  
+
+//Silver Challenge
+  //Write a series of console.log statements that acess different parts of the fishersYelpRestaurants object
+  //Write about 5-7 different things to the console
+  
+//Gold Challenge
+  //Write a couple loops that loop different properties in the  restaurantsByRating array.
+  
+    //1 - Paul's Thai Buffet - Paul's Thai 
+    //2 - Paul's Real Tacos - Carnitas
+    //3 - Paul's Pizza Palace - Stuffed
+    //4 - Paul's Indian - Paneer Paulak
+  
+  
+var fishersYelpRestaurants = {
+  directoryIdByZip     : 46032,
+  name               : "Local Yelp Directory",
+  description        : "This is a directory of restaurants in Fishers.",
+    restaurantInfo   : {
+      restaurantsByRating: [
+        { rating: 1, restaurantName: "Paul's Thai Buffet", topDish: "Paul Thai"},
+        { rating: 2, restaurantName: "Paul's Real Tacos", topDish: "Carnitas"},
+        { rating: 3, restaurantName: "Paul's Pizza Palace", topDish: "Stuffed"},
+        { rating: 4, restaurantName: "Paul's Indian", topDish: "Paneer Paulak"}
+      ]
+     }
+};
+
+console.log(fishersYelpRestaurants);
+console.log(fishersYelpRestaurants.directoryIdByZip);
+console.log(fishersYelpRestaurants.restaurantName);
+console.log(fishersYelpRestaurants.description);
+console.log(fishersYelpRestaurants.restaurantInfo.restaurantsByRating[0].restaurantName);
+
+for (var restaurant in fishersYelpRestaurants.restaurantInfo.restaurantsByRating){
+  console.log(fishersYelpRestaurants.restaurantInfo.restaurantsByRating[restaurant].restaurantName);
 }
+
+for (var restaurant in fishersYelpRestaurants.restaurantInfo.restaurantsByRating){
+  console.log(fishersYelpRestaurants.restaurantInfo.restaurantsByRating[restaurant].rating + " - " + fishersYelpRestaurants.restaurantInfo.restaurantsByRating[restaurant].restaurantName + " - " + fishersYelpRestaurants.restaurantInfo.restaurantsByRating[restaurant].topDish);
+}
+
